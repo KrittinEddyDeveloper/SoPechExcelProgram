a3f3dc91f31bed5c649d47c0a4e8b829c4a50f7a
eddy-krittin <krittin@iconext.co.th>
2019-01-02 17:39:50 +0700

    add all event

---
+++ SoPechExcel/Home.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SoPechExcel
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbMonth = new System.Windows.Forms.ComboBox();
+            this.cbbYear = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbFileA = new System.Windows.Forms.TextBox();
+            this.tbFileB = new System.Windows.Forms.TextBox();
+            this.btnBrowseFileA = new System.Windows.Forms.Button();
+            this.btnBrowseFileB = new System.Windows.Forms.Button();
+            this.btnProceed = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialogA = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogB = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Month";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(376, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Year";
+            // 
+            // cbbMonth
+            // 
+            this.cbbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbMonth.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbMonth.FormattingEnabled = true;
+            this.cbbMonth.Location = new System.Drawing.Point(189, 14);
+            this.cbbMonth.Name = "cbbMonth";
+            this.cbbMonth.Size = new System.Drawing.Size(121, 33);
+            this.cbbMonth.TabIndex = 1;
+            // 
+            // cbbYear
+            // 
+            this.cbbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbYear.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbYear.FormattingEnabled = true;
+            this.cbbYear.Location = new System.Drawing.Point(435, 14);
+            this.cbbYear.Name = "cbbYear";
+            this.cbbYear.Size = new System.Drawing.Size(121, 33);
+            this.cbbYear.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Leelawadee", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "File A (1 file)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Leelawadee", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "File B (12 files)";
+            // 
+            // tbFileA
+            // 
+            this.tbFileA.Enabled = false;
+            this.tbFileA.Font = new System.Drawing.Font("Leelawadee", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFileA.Location = new System.Drawing.Point(126, 73);
+            this.tbFileA.Multiline = true;
+            this.tbFileA.Name = "tbFileA";
+            this.tbFileA.Size = new System.Drawing.Size(493, 32);
+            this.tbFileA.TabIndex = 2;
+            // 
+            // tbFileB
+            // 
+            this.tbFileB.Enabled = false;
+            this.tbFileB.Font = new System.Drawing.Font("Leelawadee", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFileB.Location = new System.Drawing.Point(126, 132);
+            this.tbFileB.Multiline = true;
+            this.tbFileB.Name = "tbFileB";
+            this.tbFileB.Size = new System.Drawing.Size(493, 283);
+            this.tbFileB.TabIndex = 2;
+            // 
+            // btnBrowseFileA
+            // 
+            this.btnBrowseFileA.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnBrowseFileA.Font = new System.Drawing.Font("Leelawadee", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseFileA.Location = new System.Drawing.Point(625, 72);
+            this.btnBrowseFileA.Name = "btnBrowseFileA";
+            this.btnBrowseFileA.Size = new System.Drawing.Size(103, 34);
+            this.btnBrowseFileA.TabIndex = 3;
+            this.btnBrowseFileA.Text = "Browse";
+            this.btnBrowseFileA.UseVisualStyleBackColor = false;
+            this.btnBrowseFileA.Click += new System.EventHandler(this.btnBrowseFileA_Click);
+            // 
+            // btnBrowseFileB
+            // 
+            this.btnBrowseFileB.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnBrowseFileB.Font = new System.Drawing.Font("Leelawadee", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseFileB.Location = new System.Drawing.Point(625, 132);
+            this.btnBrowseFileB.Name = "btnBrowseFileB";
+            this.btnBrowseFileB.Size = new System.Drawing.Size(103, 34);
+            this.btnBrowseFileB.TabIndex = 3;
+            this.btnBrowseFileB.Text = "Browse";
+            this.btnBrowseFileB.UseVisualStyleBackColor = false;
+            this.btnBrowseFileB.Click += new System.EventHandler(this.btnBrowseFileB_Click);
+            // 
+            // btnProceed
+            // 
+            this.btnProceed.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnProceed.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProceed.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnProceed.Location = new System.Drawing.Point(263, 436);
+            this.btnProceed.Name = "btnProceed";
+            this.btnProceed.Size = new System.Drawing.Size(185, 44);
+            this.btnProceed.TabIndex = 3;
+            this.btnProceed.Text = "Proceed";
+            this.btnProceed.UseVisualStyleBackColor = false;
+            this.btnProceed.Click += new System.EventHandler(this.btnProceed_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.cbbMonth);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbbYear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(740, 61);
+            this.panel1.TabIndex = 4;
+            // 
+            // openFileDialogA
+            // 
+            this.openFileDialogA.FileName = "openFileDialog1";
+            // 
+            // openFileDialogB
+            // 
+            this.openFileDialogB.FileName = "openFileDialog2";
+            this.openFileDialogB.Multiselect = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(155, 435);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(430, 48);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(740, 498);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnProceed);
+            this.Controls.Add(this.btnBrowseFileB);
+            this.Controls.Add(this.btnBrowseFileA);
+            this.Controls.Add(this.tbFileB);
+            this.Controls.Add(this.tbFileA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Home";
+            this.ShowIcon = false;
+            this.Text = "SePech Excel Program";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbMonth;
+        private System.Windows.Forms.ComboBox cbbYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbFileA;
+        private System.Windows.Forms.TextBox tbFileB;
+        private System.Windows.Forms.Button btnBrowseFileA;
+        private System.Windows.Forms.Button btnBrowseFileB;
+        private System.Windows.Forms.Button btnProceed;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialogA;
+        private System.Windows.Forms.OpenFileDialog openFileDialogB;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ SoPechExcel/Home.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SoPechExcel.Model;
+
+namespace SoPechExcel
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            InitalComboBoxMonth();
+            InitalComboBoxYear();
+
+        }
+
+        public void InitalComboBoxMonth()
+        {
+            List<ComboBoxItem> datasource = new List<ComboBoxItem>();
+            datasource.Add(new ComboBoxItem() { Text = "-- Please Select --", Value = "0" });
+            datasource.Add(new ComboBoxItem() { Text = "January", Value = "1" });
+            datasource.Add(new ComboBoxItem() { Text = "February", Value = "2" });
+            datasource.Add(new ComboBoxItem() { Text = "March", Value = "3" });
+            datasource.Add(new ComboBoxItem() { Text = "April", Value = "4" });
+            datasource.Add(new ComboBoxItem() { Text = "May", Value = "5" });
+            datasource.Add(new ComboBoxItem() { Text = "June", Value = "6" });
+            datasource.Add(new ComboBoxItem() { Text = "July", Value = "7" });
+            datasource.Add(new ComboBoxItem() { Text = "August", Value = "8" });
+            datasource.Add(new ComboBoxItem() { Text = "September", Value = "9" });
+            datasource.Add(new ComboBoxItem() { Text = "October", Value = "10" });
+            datasource.Add(new ComboBoxItem() { Text = "November", Value = "11" });
+            datasource.Add(new ComboBoxItem() { Text = "December", Value = "12" });
+
+            cbbMonth.DisplayMember = "Text";
+            cbbMonth.ValueMember = "Value";
+            cbbMonth.DataSource = datasource;
+            cbbMonth.SelectedValue = DateTime.Today.Month.ToString();
+        }
+
+        public void InitalComboBoxYear()
+        {
+            List<ComboBoxItem> datasource = new List<ComboBoxItem>();
+            datasource.Add(new ComboBoxItem() { Text = "-- Please Select --", Value = "0" });
+            for (int i = 2018; i <= 2025; i++)
+            {
+                datasource.Add(new ComboBoxItem() { Text = i.ToString(), Value = i.ToString() });
+            }
+
+            cbbYear.DisplayMember = "Text";
+            cbbYear.ValueMember = "Value";
+            cbbYear.DataSource = datasource;
+            cbbYear.SelectedValue = DateTime.Today.Year.ToString();
+        }
+
+        private void btnBrowseFileA_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialogA.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                try
+                {
+                    tbFileA.Text = openFileDialogA.FileName;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void btnBrowseFileB_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialogB.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                try
+                {
+                    tbFileB.Text = string.Join(Environment.NewLine, openFileDialogB.FileNames.ToList());
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void btnProceed_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(openFileDialogA.FileName))
+            {
+                MessageBox.Show(this, @"Please Select File A !!!", @"Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (openFileDialogB.FileNames == null || !openFileDialogB.FileNames.Any())
+            {
+                MessageBox.Show(this, @"Please Select File B !!!", @"Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            progressBar1.Visible = true;
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Visible = false;
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                MessageBox.Show(@"Cancel");
+            }
+            else
+            {
+                MessageBox.Show(this, @"Proceed Success.", @"Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
